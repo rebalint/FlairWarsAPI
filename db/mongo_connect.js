4c40ed81967a5ea6311c48e31116cbb8068fa552
nate-e-97 <nathanieleeten@gmail.com>
2020-12-31 08:22:09 -0700

    Mongoose problem fix

--- db/mongo_connect.js
+++ db/mongo_connect.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
-
+const AppSchema = require('./schema/application')
+const AssignmentSchema = require('./schema/assignment')
+const CurrencySchema = require('./schema/currency')
+const FamilySchema = require('./schema/family')
+const GuildSchema = require('./schema/guild')
+const ItemSchema = require('./schema/item')
+const TitleSchema = require('./schema/title')
+const UserSchema = require('./schema/user')
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
 const db = mongoose.connection;
